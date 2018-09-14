24f487bdb4e0e3e854834111c2673f019c7a9949
Luis.Valle <luis.valle@almundo.com>
2018-09-13 21:40:00 -0500

    Operator

--- scripts/05-operators.js
+++ scripts/05-operators.js
@@ -10,3 +10,28 @@ console.log("Incremento ++2: " + (++numero))
 console.log("Decremento 3--: " + (--numero))
 console.log("Unario + '3': " + (+3))
 console.log("Unario + true: " + (+true))
+
+console.log("------------------------Operadores de asignacion------------------------")
+numero = 9
+numero += 3;
+console.log("operador de asignacion 9+=3: " + numero)
+console.log("Aplica tambien para -=,/=, *=")
+
+console.log("------------------------Operadores bit------------------------")
+console.log("true & true " + (true & true))
+console.log("true & false " + (true & false))
+console.log("true | true " + (true | true))
+console.log("true | false " + (true | false))
+console.log("~2 (-(x + 1)): " + (-3))
+
+
+console.log("------------------------Operadores comparacion------------------------")
+console.log("1 == 1: " + (1 == 1));
+console.log("\"1\" == 1: " + ("1" == 1));
+console.log("1 === 1: " + (1 === 1));
+console.log("'1'===1: " + ("1" === 1));
+var object1 = {'a': 'b'}, object2 = {'a': 'b'};
+console.log("object1 = {'a': 'b'} == object2 = {'a': 'b'}: " + (object1 == object2));
+console.log("1 !=   2: " + (1 != 2))
+console.log("1 <  2: " + (1 < 2))
+console.log("1 >  2: " + (1 > 2))
